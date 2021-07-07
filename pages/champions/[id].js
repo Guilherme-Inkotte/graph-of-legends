46031de13b323f7f96991e718924d0f96e141b3a
guilherme-inkotte <gui.inkotte@hotmail.com>
2021-07-06 21:58:10 -0300

    Initial structure stablished

---
+++ pages/champions/[id].js
@@ -0,0 +1,9 @@
+const ChampionDetails = () => {
+    return (
+        <div>
+            <h1>Campeão</h1>
+        </div>
+    );
+}
+ 
+export default ChampionDetails;
---
+++ pages/champions/index.js
@@ -0,0 +1,9 @@
+const ChampionsList = () => {
+    return ( <main>
+        <p>Aatrox</p>
+        <p>Akali</p>
+        <p>Ashe</p>
+    </main> );
+}
+ 
+export default ChampionsList;
